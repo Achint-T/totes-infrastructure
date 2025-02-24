aa597ebc48814d065680fbaaac2f6562adb4cb2c
Jneats <sophiaphoon@gmail.com>
2025-02-24 16:54:51 +0000

    Test changes to see if cicd pipeline pushes terraform code

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket" "s3_ingestion_bucket" {
     bucket_prefix = "${var.S3_BUCKET_PREFIX}-"
 }
 
-resource "aws_s3_bucket" "s3_ingestion_bucket" {
+resource "aws_s3_bucket" "s3_ingestion_bucket-test" {
     bucket_prefix = "test-s3-bucket-for-totes-infra"
 }
