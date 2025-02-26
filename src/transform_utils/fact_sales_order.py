d872f17fb4e19d8746e774461e9cf59e3a82521e
Jneats <sophiaphoon@gmail.com>
2025-02-26 12:11:49 +0000

    Added error handling and tests to fact_sales_order. Also added test to check that columns have been transformed.

--- src/transform_utils/fact_sales_order.py
+++ src/transform_utils/fact_sales_order.py
@@ -1,34 +1,67 @@
 import pandas as pd
 
-# Inputs: Pandas dataframe for sales_order 
+# Inputs: Pandas dataframe for sales_order
 # Create new dataframe with data that has been transformed
-# Return new dataframe 
-
+# Return new dataframe
 
 
 def util_fact_sales_order(df_sales_order):
+
     df_fact_sales_order = pd.DataFrame()
 
-    # df_fact_sales_orders['sales_record_id'] = df_sales_order['sales_record_id']
-    df_fact_sales_order['sales_order_id'] = df_sales_order['sales_order_id']
+    required_columns = [
+        "sales_order_id",
+        "created_at",
+        "last_updated",
+        "staff_id",
+        "counterparty_id",
+        "units_sold",
+        "unit_price",
+        "currency_id",
+        "design_id",
+        "agreed_payment_date",
+        "agreed_delivery_date",
+        "agreed_delivery_location_id",
+    ]
 
-    df_fact_sales_order['created_date'] = pd.to_datetime(df_sales_order['created_at']).dt.date
-    df_fact_sales_order['created_time'] = pd.to_datetime(df_sales_order['created_at']).dt.time
-    df_fact_sales_order['last_updated_date'] = pd.to_datetime(df_sales_order['last_updated']).dt.date
-    df_fact_sales_order['last_updated_time'] = pd.to_datetime(df_sales_order['last_updated']).dt.time
+    # Error handling: empty dataframe, missing columns
+    if df_sales_order.empty:
+        return "The source dataframe for fact_sales_order is empty"
+    col_missing = [col for col in required_columns if col not in df_sales_order.columns]
+    if col_missing:
+        return f"Error: Missing columns {', '.join(col_missing)}"
 
-    df_fact_sales_order['sales_staff_id'] = df_sales_order['staff_id']
-    df_fact_sales_order['counterparty_id'] = df_sales_order['counterparty_id']
-    df_fact_sales_order['units_sold'] = df_sales_order['units_sold']
-    df_fact_sales_order['unit_price'] = pd.to_numeric(df_sales_order['unit_price']).round(2)
-    df_fact_sales_order['currency_id'] = df_sales_order['currency_id']
-    df_fact_sales_order['design_id'] = df_sales_order['design_id']
+    # Error handling: Added errors='coerce' to columns with date, time or numeric values. Values for date/time become NaT if they are not a time. 
+    df_fact_sales_order["sales_order_id"] = df_sales_order["sales_order_id"]
 
-    df_fact_sales_order['agreed_payment_date'] = pd.to_datetime(df_sales_order['agreed_payment_date']).dt.date
-    df_fact_sales_order['agreed_delivery_date'] = pd.to_datetime(df_sales_order['agreed_delivery_date']).dt.date
-    df_fact_sales_order['agreed_delivery_location_id'] = df_sales_order['agreed_delivery_location_id']
+    df_fact_sales_order["created_date"] = pd.to_datetime(df_sales_order["created_at"], errors='coerce').dt.date
+    df_fact_sales_order["created_time"] = pd.to_datetime(
+        df_sales_order["created_at"], errors='coerce'
+    ).dt.time
+    df_fact_sales_order["last_updated_date"] = pd.to_datetime(
+        df_sales_order["last_updated"], errors='coerce'
+    ).dt.date
+    df_fact_sales_order["last_updated_time"] = pd.to_datetime(
+        df_sales_order["last_updated"], errors='coerce'
+    ).dt.time
 
-    df_fact_sales_order.index.name = 'sales_record_id'
-    
+    df_fact_sales_order["sales_staff_id"] = df_sales_order["staff_id"]
+    df_fact_sales_order["counterparty_id"] = df_sales_order["counterparty_id"]
+    df_fact_sales_order["units_sold"] = df_sales_order["units_sold"]
+    df_fact_sales_order["unit_price"] = pd.to_numeric(
+        df_sales_order["unit_price"], errors='coerce'
+    ).round(2)
+    df_fact_sales_order["currency_id"] = df_sales_order["currency_id"]
+    df_fact_sales_order["design_id"] = df_sales_order["design_id"]
+
+    df_fact_sales_order["agreed_payment_date"] = pd.to_datetime(
+        df_sales_order["agreed_payment_date"], errors='coerce'
+    ).dt.date
+    df_fact_sales_order["agreed_delivery_date"] = pd.to_datetime(
+        df_sales_order["agreed_delivery_date"], errors='coerce'
+    ).dt.date
+    df_fact_sales_order["agreed_delivery_location_id"] = df_sales_order["agreed_delivery_location_id"]
+
+    df_fact_sales_order.index.name = "sales_record_id"
+
     return df_fact_sales_order
-
