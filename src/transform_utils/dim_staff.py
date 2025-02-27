747255f5899b64324af7d85e04d1fca61d0522c0
Fiona Coutts <fionaecoutts@gmail.com>
2025-02-27 10:06:59 +0000

    remove df_fact_sales_order and get staff_id from passed staff table. also select all fields from merged table when constructing new df

--- src/transform_utils/dim_staff.py
+++ src/transform_utils/dim_staff.py
@@ -4,27 +4,22 @@ import pandas as pd
 # Create new dataframe with data that has been transformed
 # Return new dataframe 
 
-def util_dim_staff(df_fact_sales_order, df_staff, df_department):
+def util_dim_staff(df_staff, df_department):
     #error handling for empty dataframes and missing columns. raising errors
     #instead of only returning messages (apart from for df_fact_sales_order where i couldn't get this to work)
     
-    if df_fact_sales_order.empty:
-        return "The source dataframe for df_fact_sales_order is empty"
     if df_staff.empty:
         raise ValueError("The source dataframe for df_staff is empty")
     if df_department.empty:
         raise ValueError("The source dataframe for df_department is empty")
 
-    required_columns_fact_sales = {"staff_id"}
-    required_columns_staff = {"first_name", "last_name", "email_address", "department_id"}
+    required_columns_staff = {"staff_id", "first_name", "last_name", "email_address", "department_id"}
     required_columns_department = {"department_id", "department_name", "location"}
 
-    missing_fact_sales = required_columns_fact_sales - set(df_fact_sales_order.columns)
     missing_staff = required_columns_staff - set(df_staff.columns)
     missing_department = required_columns_department - set(df_department.columns)
 
-    if missing_fact_sales:
-        raise KeyError(f"Missing columns in df_fact_sales_order: {', '.join(missing_fact_sales)}")
+    
     if missing_staff:
         raise KeyError(f"Missing columns in df_staff: {', '.join(missing_staff)}")
     if missing_department:
@@ -34,15 +29,14 @@ def util_dim_staff(df_fact_sales_order, df_staff, df_department):
     df_merged = df_staff.merge(df_department, on='department_id', how='left')
     df_dim_staff = pd.DataFrame()
     
-    df_dim_staff['staff_id'] = df_fact_sales_order['staff_id'].astype(int)
+    df_dim_staff['staff_id'] = df_merged['staff_id'].astype(int)
 
-    df_dim_staff['first_name'] = df_staff['first_name'].astype(str)
-    df_dim_staff['last_name'] = df_staff['last_name'].astype(str)
-    df_dim_staff['email_address'] = df_staff['email_address'].astype(str)
+    df_dim_staff['first_name'] = df_merged['first_name'].astype(str)
+    df_dim_staff['last_name'] = df_merged['last_name'].astype(str)
+    df_dim_staff['email_address'] = df_merged['email_address'].astype(str)
 
     df_dim_staff['department_name'] = df_merged['department_name'].astype(str)
     df_dim_staff['location'] = df_merged['location'].astype(str)
-    
     
     return df_dim_staff
 
