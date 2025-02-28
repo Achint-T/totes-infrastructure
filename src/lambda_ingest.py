7233a8395b80ede04f9042d994bc7cf39447093c
jryusuf <yusufyuksel@aol.com>
2025-02-28 11:45:35 +0000

    updated lambda handler

--- src/lambda_ingest.py
+++ src/lambda_ingest.py
@@ -1,43 +1,61 @@
 import boto3
-from helpers import fetch_credentials, export_db_creds_to_env
-from ingestion_utils.file_utils import data_to_csv
-from ingestion_utils.database_utils import create_connection, close_db_connection, get_recent_additions, get_last_upload_date
+from src.helpers import fetch_credentials, export_db_creds_to_env
+from src.ingestion_utils.file_utils import data_to_csv, get_current_time
+from src.ingestion_utils.database_utils import create_connection, close_db_connection, get_recent_additions, get_last_upload_date
 import time
 import logging
 
-def lambda_handler(event,context):
-    #csv key for s3
-    #update the lastupdatedate in secrets manager
+secret_client =  boto3.client("secretsmanager")
+s3_client = boto3.client("s3")
+
+def lambda_handler(event, context):
+    """Handles the lambda function invocation.
+
+    Extracts data from specified tables, saves it to CSV, and uploads to S3.
+
+    Args:
+        event (dict): Event data passed to the lambda function. Must contain 'tables' key.
+        context (object): Lambda context object (not used in this function).
+
+    Returns:
+        dict: Status code and message indicating success or failure.
+    """
+    conn = None
     try:
-        
-        secret_client =  boto3.client("secretsmanager")
-        db_creds = fetch_credentials(secret_client, secret_name="database_credentials")
-        export_db_creds_to_env(db_creds, ["username","password","port","host"])
+        tables_to_ingest = event["tables"]
+        conn = get_connection()
+        last_date = get_last_run_date()
+        timestamp = get_current_time(time.gmtime())
+        save_data_to_s3(conn, tables_to_ingest, last_date, timestamp)
 
-        conn = create_connection()
+        logging.info(f"Extraction run successfully for tables: {tables_to_ingest}")
+        return {"statusCode": 200, "body": "Extraction run successfully"}
 
-        last_date = get_last_upload_date(secret_client)
-
-        
-        tables_to_ingest = ['sales_order', 'design', 'currency', 'staff', 'counterparty', 'address', 'department', 'purchase_order', 'payment_type', 'payment', 'transaction']
-        
-        timenow = list(time.gmtime()[:5]) + [time.gmtime()[6]-1]
-        date = '-'.join([str(number).rjust(2,'0') for number in timenow[:3]])
-        hours = ':'.join([str(number).rjust(2,'0') for number in timenow[3:]])
-        timestamp = f'{date} {hours}'
-
-        for table in tables_to_ingest:
-            data = get_recent_additions(conn, tablename=table, updatedate=last_date, time_now=timestamp)
-            data_to_csv(data, table_name=table)
-            s3_client = boto3.client("s3")
-            s3_client.upload_file(f"./{table}.csv", "s3-totes-sys-ingestion-bucket-20250227154311511600000002", '/'.join(map(str,timenow)) + '/' + table + '.csv')
-
-
-        close_db_connection(conn)
-        return {"statusCode": 200, "body": "placeholder lambda executed successfully"}
+    except KeyError as ke:
+        logging.error(f"Missing key in event: {ke}")
+        #Reconsider suitable status code
+        return {"statusCode": 400, "body": f"Missing table information: {ke}"}
     except Exception as e:
-        #close_db_connection(conn)
-        logging.info(msg=f"failed {e}")
-        return ""
-    
-lambda_handler({},{})
+        logging.error(f"Extraction run failed: {e}")
+        return {"statusCode": 500, "body": f"Extraction run failed: {e}"} 
+    finally:
+        if conn:
+            close_db_connection(conn)
+
+def get_connection(secret_client = secret_client):
+    db_creds = fetch_credentials(secret_client, secret_name="database_credentials")
+    export_db_creds_to_env(db_creds, ["username","password","port","host"])
+    conn = create_connection()
+    return conn
+
+def get_last_run_date(secret_client = secret_client):
+    return get_last_upload_date(secret_client)
+
+def save_data_to_s3(conn, tables_to_ingest,last_date, timestamp, s3_client=s3_client,):
+    for table in tables_to_ingest:
+        data = get_recent_additions(conn, tablename=table, updatedate=last_date, time_now=timestamp["secret"])
+        data_to_csv(data, table_name=table)
+        s3_client.upload_file(f"./{table}.csv", "s3-totes-sys-ingestion-bucket-20250227154311511600000002", timestamp["filepath"] + '/' + table + '.csv')
+
+
+#lambda_handler(event={"tables":["sales_order", "design"]},context={})
