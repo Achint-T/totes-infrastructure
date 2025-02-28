34024921f248c4f93b71e0739f5dbbd48a145be2
jryusuf <yusufyuksel@aol.com>
2025-02-28 14:35:08 +0000

    added secrets manager acces policy to lambda

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -193,3 +193,26 @@ resource "aws_iam_role_policy_attachment" "state_machine_logs_attachment" {
   role       = aws_iam_role.state_machine_role.name
   policy_arn = aws_iam_policy.state_machine_cloudwatch_logs_policy.arn
 }
+
+resource "aws_iam_policy" "secretsmanager_access_policy" {
+  name        = "secretsmanager-access-policy"
+  description = "Policy to allow Lambda function to access Secrets Manager"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret" 
+        ],
+        Effect = "Allow",
+        Resource = ["*"]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_secretsmanager_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.secretsmanager_access_policy.arn
+}
