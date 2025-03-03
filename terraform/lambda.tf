5ee7502b09b371340b524c0640ee20a0c5a690c2
redbramble <victoriabramble2@gmail.com>
2025-03-03 13:58:21 +0000

    Branch changes again

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -65,13 +65,13 @@ resource "aws_lambda_function" "ingestion_handler" {
   runtime          = "python3.12"
   timeout          = 60
 
-#   environment {
-#     variables = {
-#       BUCKET_NAME = aws_s3_bucket.s3_ingestion_bucket.bucket
-#     }
-#   }
-#   depends_on = [ aws_s3_bucket.s3_ingestion_bucket ]
-# }
+  # environment {
+  #   variables = {
+  #     BUCKET_NAME = aws_s3_bucket.s3_ingestion_bucket.bucket
+  #   }
+  # }
+  # depends_on = [ aws_s3_bucket.s3_ingestion_bucket ]
+}
 
 # Transform lambda 
 
