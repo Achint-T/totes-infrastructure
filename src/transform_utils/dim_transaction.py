9b31701b1964ce462faafea8f3bd83007e88b24a
redbramble <victoriabramble2@gmail.com>
2025-03-05 10:30:28 +0000

    Added full unit tests for dim transaction util and finished function.

--- src/transform_utils/dim_transaction.py
+++ src/transform_utils/dim_transaction.py
@@ -1,4 +1,34 @@
 import pandas as pd
 
 def util_dim_transaction(df_transaction):
-    pass
+    """Performs transformation on input dataframe to convert it to suitable
+       structure for data warehouse.
+       
+       Args:
+       df_transaction: The dataframe created from the transaction table csv.
+       
+       Returns:
+       df_dim_transaction - a dataframe ready to be converted to parquet file."""
+    
+    df_dim_transaction = pd.DataFrame()
+
+    if df_transaction.empty:
+        return "Error: The source transaction dataframe is empty"
+    
+    required_columns = [
+        "transaction_id",
+        "transaction_type",
+        "sales_order_id",
+        "purchase_order_id"
+    ]
+
+    col_missing = [col for col in required_columns if col not in df_transaction.columns]
+    if col_missing:
+        return f"Error: Missing columns {', '.join(col_missing)} for the source transaction dataframe"
+    
+    df_dim_transaction["transaction_id"] = df_transaction["transaction_id"]
+    df_dim_transaction["transaction_type"] = df_transaction["transaction_type"]
+    df_dim_transaction["sales_order_id"] = df_transaction["sales_order_id"]
+    df_dim_transaction["purchase_order_id"] = df_transaction["purchase_order_id"]
+
+    return df_dim_transaction
--- test/test_dim_transaction.py
+++ test/test_dim_transaction.py
@@ -21,17 +21,22 @@ def transaction():
     return df_test_transaction
 
 @pytest.fixture
-def transaction_missing_columns(transaction):
+def transaction_missing_required_columns(transaction):
     df_missing_cols = transaction.drop(columns=["transaction_id"])
     return df_missing_cols
 
 @pytest.fixture
-def transaction_with_blanks(transaction):
+def transaction_missing_unnecessary_columns(transaction):
+    df_missing_cols = transaction.drop(columns=["last_updated"])
+    return df_missing_cols
+
+@pytest.fixture
+def transaction_with_blanks():
     data = {
         "transaction_id": [1, 2, 3],
         "transaction_type": ["PURCHASE", "PURCHASE", "SALE"],
-        "sales_order_id": [None, None, 3],
-        "purchase_order_id": [1, 2, None],
+        "sales_order_id": [None, 2, 3],
+        "purchase_order_id": [None, 2, 3],
         "created_at": ["2022-11-03 14:20:52.186",
             "2022-11-03 14:20:52.186",
             "2022-11-03 14:20:52.186"],
@@ -41,3 +46,45 @@ def transaction_with_blanks(transaction):
     }
     df_with_blanks = pd.DataFrame(data)
     return df_with_blanks
+
+class TestTransaction:
+
+    def test_returns_dataframe(self, transaction):
+        output = util_dim_transaction(transaction)
+        assert isinstance(output, pd.DataFrame)
+
+    def test_returns_correct_columns(self, transaction):
+        output = util_dim_transaction(transaction)
+        assert list(output.columns) == [
+        "transaction_id",
+        "transaction_type",
+        "sales_order_id",
+        "purchase_order_id"]
+
+    def test_returns_empty_values_where_acceptable(self, transaction_with_blanks):
+        output = util_dim_transaction(transaction_with_blanks)
+
+        assert pd.isnull(output["sales_order_id"].iloc[0])
+        assert pd.isnull(output["purchase_order_id"].iloc[0])
+
+    def test_returns_correct_columns_when_unneccesary_column_missing(self, transaction_missing_unnecessary_columns):
+        output = util_dim_transaction(transaction_missing_unnecessary_columns)
+
+        assert list(output.columns) == [
+        "transaction_id",
+        "transaction_type",
+        "sales_order_id",
+        "purchase_order_id"]
+
+class TestTransactionErrors:
+
+    def test_empty_dataframe(self):
+        df_blank = pd.DataFrame()
+        output = util_dim_transaction(df_blank)
+
+        assert output == "Error: The source transaction dataframe is empty"
+
+    def test_required_column_missing(self, transaction_missing_required_columns):
+        output = util_dim_transaction(transaction_missing_required_columns)
+
+        assert output == "Error: Missing columns transaction_id for the source transaction dataframe"
