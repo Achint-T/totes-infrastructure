7333f5fcff65ca2ee2f39c5b316978280d76acf3
Achint Thakkar <achinthakkar@outlook.com>
2025-03-05 15:02:40 +0000

    new transform util - fact_purchase_order

--- src/ingestion_utils/database_utils.py
+++ src/ingestion_utils/database_utils.py
@@ -88,7 +88,7 @@ def put_last_upload_date(time_object, secretclient):
     Takes in the timeobject from time.gmtime() or a list in the form [year,month,day,hour,minute,second] and secret client from boto3.client
         - if the secrets exsists it updates it otherwise creates a new secret called 'lastupload'
     returns nothing
-    raises an error if it fails to put the secret e.g. invalid clientse
+    raises an error if it fails to put the secret e.g. invalid client
     '''
 
     date = '-'.join([str(number).rjust(2,'0') for number in time_object[:3]])
