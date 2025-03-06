fc1d9ac6af68d359b0d1689195fd955d6e01760d
redbramble <victoriabramble2@gmail.com>
2025-03-06 15:35:07 +0000

    Added db and wr credentials secrets to terraform, added github secrets, and updated yml file accordingly.

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -6,3 +6,21 @@ resource "aws_secretsmanager_secret_version" "lastupload_version" {
   secret_id     = aws_secretsmanager_secret.lastupload.id
   secret_string = var.lastupload_value
 }
+
+resource "aws_secretsmanager_secret" "database_credentials" {
+    name = "database_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "database_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.database_credentials.id
+  secret_string = var.database_credentials_value
+}
+
+resource "aws_secretsmanager_secret" "warehouse_credentials" {
+    name = "warehouse_credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "warehouse_credentials_version" {
+  secret_id     = aws_secretsmanager_secret.warehouse_credentials.id
+  secret_string = var.warehouse_credentials_value
+}
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -3,3 +3,15 @@ variable "lastupload_value" {
   type        = string
   default     = "2020-01-01 00:00:00"
 }
+
+variable "database_credentials_value" {
+  description = "Database credentials for the totesys database"
+  type        = string
+  sensitive   = true
+}
+
+variable "warehouse_credentials_value" {
+  description = "Database credentials for the final warehouse"
+  type        = string
+  sensitive   = true
+}
