c07eb941483a28b8d02ae7fafb15ec62bcc24955
redbramble <victoriabramble2@gmail.com>
2025-03-07 09:42:23 +0000

    Moved lastupdated secret to terraform-no-deletion directory.

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,12 +1,3 @@
-resource "aws_secretsmanager_secret" "lastupload" {
-  name = "lastupload"
-}
-
-resource "aws_secretsmanager_secret_version" "lastupload_version" {
-  secret_id     = aws_secretsmanager_secret.lastupload.id
-  secret_string = var.lastupload_value
-}
-
 resource "aws_secretsmanager_secret" "database_credentials" {
     name = "database_credentials"
 }
--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,9 +1,3 @@
-variable "lastupload_value" {
-  description = "The last upload timestamp"
-  type        = string
-  default     = "2020-01-01 00:00:00"
-}
-
 variable "database_credentials_value" {
   description = "Database credentials for the totesys database"
   type        = string
