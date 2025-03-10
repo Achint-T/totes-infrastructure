f8368c993912e4032b55b346761e37748bfceb86
Fiona Coutts <fionaecoutts@gmail.com>
2025-03-10 13:55:16 +0000

    single quotes to double quotes around dim_tables

--- src/lambda_transform.py
+++ src/lambda_transform.py
@@ -27,8 +27,8 @@ Ingestion lambda will return a dictionary in the form:
 
 {
 "status_code": 200,
-"fact_tables":{"sales_order": "2025/6/3/16/47/sales_order.csv",...}, 
-"dim_tables":{"staff": "2025/6/3/16/47/staff", "department": "2025/6/3/16/47/department", ...}
+"fact_tables':{'sales_order': '2025/6/3/16/47/sales_order.csv',...}, 
+'dim_tables':{'staff': '2025/6/3/16/47/staff', 'department': '2025/6/3/16/47/department', ...}
 }
 
 The transform lambda will take this as its EVENT and run transformation utils on all the
@@ -42,26 +42,26 @@ created parquet files in transformed bucket.
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-s3_client = boto3.client("s3")
+s3_client = boto3.client('s3')
 ingestion_bucket = os.environ["INGESTION_BUCKET_NAME"]
 transformed_bucket = os.environ["TRANSFORMED_BUCKET_NAME"]
 
 def lambda_handler(event,context):
     """Event example:
-    {"fact_tables":{"sales_order": "2025/6/3/16/47/sales_order.csv",...}, 
-    "dim_tables":{"staff": "2025/6/3/16/47/staff", "department": "2025/6/3/16/47/department", ...}}
+    {'fact_tables':{'sales_order': '2025/6/3/16/47/sales_order.csv',...}, 
+    'dim_tables':{'staff': '2025/6/3/16/47/staff', 'department': '2025/6/3/16/47/department', ...}}
 
     Returns dictionary of same format of event with filepaths to parquet files.
 
     Context not needed for this function. Can be anything
-    """"
+    '"""
     # logger.info(f"Received event: {event}")
     # if not isinstance(event, dict):
     #     raise TypeError("event must be a dictionary")
     # if "fact_tables" not in event:
-    #     raise ValueError("event must contain "fact_tables"")
+    #     raise ValueError('event must contain "fact_tables"')
     # if "dim_tables" not in event:
-    #     raise ValueError("event must contain "dim_tables"")
+    #     raise ValueError('event must contain "dim_tables"')
     #
 
     try:
@@ -100,13 +100,13 @@ def lambda_handler(event,context):
 
 def run_dim_utils(event, ingestion_bucket):
     """runs dim utils for each of the passed dim_tables from the event. returns transformed dataframes"""
-    table_relations = {"fact_sales_order": ["sales_order"],
-                "dim_staff": ["staff","department"],
-                "dim_counterparty": ["counterparty", "address"],
-                "dim_currency": ["currency"],
-                "dim_date": ["date"],
-                "dim_design": ["design"],
-                "dim_location": ["address"]}
+    table_relations = {'fact_sales_order': ['sales_order'],
+                'dim_staff': ['staff','department'],
+                'dim_counterparty': ['counterparty', 'address'],
+                'dim_currency': ['currency'],
+                'dim_date': ['date'],
+                'dim_design': ['design'],
+                'dim_location': ['address']}
     
     transformed_dfs = {} 
     dfs = {}
@@ -121,18 +121,18 @@ def run_dim_utils(event, ingestion_bucket):
     for dim_table, required_tables in table_relations.items():
         try:
             if all(tbl in dfs for tbl in required_tables):
-                if dim_table == "dim_staff":
-                    transformed_dfs[dim_table] = util_dim_staff(dfs["staff"], dfs["department"])
-                elif dim_table == "dim_counterparty":
-                    transformed_dfs[dim_table] = util_dim_counterparty(dfs["counterparty"], dfs["address"])
-                elif dim_table == "dim_currency":
-                    transformed_dfs[dim_table] = util_dim_currency(dfs["currency"])
-                elif dim_table == "dim_location":
-                    transformed_dfs[dim_table] = util_dim_location(dfs["address"])
-                elif dim_table == "dim_date":
-                    transformed_dfs[dim_table] = util_dim_date(dfs["date"])
-                elif dim_table == "dim_design":
-                    transformed_dfs[dim_table] = util_dim_design(dfs["design"])
+                if dim_table == 'dim_staff':
+                    transformed_dfs[dim_table] = util_dim_staff(dfs['staff'], dfs['department'])
+                elif dim_table == 'dim_counterparty':
+                    transformed_dfs[dim_table] = util_dim_counterparty(dfs['counterparty'], dfs['address'])
+                elif dim_table == 'dim_currency':
+                    transformed_dfs[dim_table] = util_dim_currency(dfs['currency'])
+                elif dim_table == 'dim_location':
+                    transformed_dfs[dim_table] = util_dim_location(dfs['address'])
+                elif dim_table == 'dim_date':
+                    transformed_dfs[dim_table] = util_dim_date(dfs['date'])
+                elif dim_table == 'dim_design':
+                    transformed_dfs[dim_table] = util_dim_design(dfs['design'])
 
                 logger.info(f"successfully transformed {dim_table}")
             else:
@@ -148,10 +148,10 @@ def run_fact_utils(event, ingestion_bucket):
     transformed_dfs = {} 
     dfs = {}
 
-    for table in event["fact_tables"]:
+    for table in event['fact_tables']:
         try:
-            if event["fact_tables"][table]:
-                dfs[table] = read_csv_from_s3(ingestion_bucket, event["fact_tables"][table])
+            if event['fact_tables'][table]:
+                dfs[table] = read_csv_from_s3(ingestion_bucket, event['fact_tables'][table])
             else:
                 logger.info(f"No passed csv file for {table}")
         except Exception as e:
@@ -160,12 +160,12 @@ def run_fact_utils(event, ingestion_bucket):
 
     for df in dfs:
         try:
-            if df == "sales_order":
-                transformed_dfs[f"fact_{df}"] = util_fact_sales_order(dfs["sales_order"])
-            elif df == "purchase_order":
-                transformed_dfs[f"fact_{df}"] = util_fact_purchase_order(dfs["purchase_order"])
-            elif df == "payment":
-                transformed_dfs[f"fact_{df}"] =  util_fact_payment(dfs["payment"])
+            if df == 'sales_order':
+                transformed_dfs[f'fact_{df}'] = util_fact_sales_order(dfs['sales_order'])
+            elif df == 'purchase_order':
+                transformed_dfs[f'fact_{df}'] = util_fact_purchase_order(dfs['purchase_order'])
+            elif df == 'payment':
+                transformed_dfs[f'fact_{df}'] =  util_fact_payment(dfs['payment'])
             logger.info(f"successfully transformed {df}")
 
         except Exception as e:
